dcf428c10a0dc056779a71a65424c4433642a6e0
Johan Svensson <plasmacheesejoj@gmail.com>
2018-09-21 14:51:11 +0200

    starting on project carousel

--- js/main.js
+++ js/main.js
@@ -3,9 +3,16 @@ import projects from "./projects.js";
 var app = new Vue({
   el: '#root',
   data: {
+    activeProjectIndex: 0,
     projects
   },
   methods: {
-    
+    nextProject: () => {
+      app.activeProjectIndex = (app.activeProjectIndex + 1) % projects.length;
+    },
+
+    prevProject: () => {
+      app.activeProjectIndex = (app.activeProjectIndex - 1) % projects.length;
+    }
   }
 });
--- js/projects.js
+++ js/projects.js
@@ -1,7 +1,13 @@
 export default [{
   title: 'Elevkårsapp',
-  description: 'Lorem ipsum'
+  description: 'Lorem ipsum',
+  coverImageSrc: 'assets/cover/studentapp.jpg'
 }, {
   title: 'Project 2',
-  description: 'Lorem ipsum'
+  description: 'Lorem ipsum',
+  coverImageSrc: 'assets/cover/studentapp.jpg'
+}, {
+  title: 'Project 3',
+  description: 'Lorem ipsum',
+  coverImageSrc: 'assets/cover/studentapp.jpg'
 }]
