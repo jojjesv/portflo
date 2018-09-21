7f23b0a4eb421c158743fa405b1583aec2bccbda
Johan Svensson <plasmacheesejoj@gmail.com>
2018-09-21 15:19:02 +0200

    added project info

--- js/projects.js
+++ js/projects.js
@@ -1,10 +1,14 @@
 export default [{
   title: 'Elevkårsapp',
+  subtitle: 'A step for student communication',
   description: 'Lorem ipsum',
   coverImageSrc: 'assets/cover/studentapp.jpg'
 }, {
   title: 'Project 2',
-  description: 'Lorem ipsum',
+  subtitle: 'Writing Technical Articles',
+  description: `The notes below apply to technical papers in computer science and electrical engineering, with emphasis on papers in systems and networks.
+  Read Strunk and White, Elements of Style. Again.<br />
+  Give the paper to somebody else to read. If you can, find two people: one person familiar with the technical matter, another only generally familiar with the area.`,
   coverImageSrc: 'assets/cover/studentapp.jpg'
 }, {
   title: 'Project 3',
